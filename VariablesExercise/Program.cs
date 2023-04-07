09158d48a014040333ba7a2bbd08d84255bb5810
yvonnehuynh79 <yvonnehuynh888@gmail.com>
2023-04-07 11:52:45 -0700

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Berkin";
+            int dogAge = 2;
+            char b = '#';
+            bool small = true;
+            double food = 1.2;
+            decimal mile = 2.4m;
+            Console.WriteLine($"My dog name is {dogName} , he is {dogAge} years old, he is {b}, \n" +
+                $"he is a {small} dog, he eats {food} pound a day, \n" +
+                $"he walks {mile} miles almost everyday"  );
+             
+
         }
     }
 }
+ 
